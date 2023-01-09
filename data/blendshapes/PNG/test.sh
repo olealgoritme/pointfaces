10701ae06c5c10554d521791475c943599381d2d
Ole Algoritme <olealgoritme@gmail.com>
2023-01-09 20:29:06 +0000

    point cloud transformed, as images

--- data/blendshapes/PNG/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for filename in *.ply; do
-    filename_no_ext=$(basename "$filename" .ply)
-    ./ply2jpg.py ${filename}
-    convert ${filename_no_ext}.png -threshold 50% -define png:color-type=1 -define png:bit-depth=1 ${filename_no_ext}.png
-done
